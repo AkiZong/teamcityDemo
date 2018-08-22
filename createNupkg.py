919a279b3150d645a763bb4273177614cfdf3e4d
Bingxia Zong <zbxaki@gmail.com>
2018-08-22 15:17:11 -0400

    Update createNupkg.py

--- createNupkg.py
+++ createNupkg.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import subprocess
 import sys
+import shutil
 from tempfile import mkstemp
 from shutil import move
 from os import fdopen, remove
@@ -14,7 +15,7 @@ def replaceVersion (nuspec_path, new_version):
     with fdopen(fh, 'w') as new_file:
         with open(nuspec_path) as old_file:
             for line in old_file:
-                new_file.write(line.replace("<version>1.0.0</version>", "<version>"+new_version+"</version>"))
+                new_file.write(line.replace('<version>1.0.0</version>', '<version>'+new_version+'</version>'))
     remove(nuspec_path)
     move(abs_path, nuspec_path)
 
@@ -24,14 +25,16 @@ def nugetPack (nuspec_path):
 	os.system(cmd)
 
 def copyNupkg (nuspkg_id, new_version):
+	if os.path.exists('package'):
+		shutil.rmtree('package')
+
 	cmd = 'mkdir package'
 	os.system(cmd)
 
-	nuspkg = nuspkg_id + "." + new_version + '.nupkg'
+	nuspkg = nuspkg_id + '.' + new_version + '.nupkg'
 	cmd = 'xcopy /Y ' + nuspkg + ' package'
 	os.system(cmd)
 
-	# xcopy /Y .\%newPkgName% .\package
 
 if __name__ == "__main__":
 	for arg in sys.argv[1:]:
@@ -45,5 +48,7 @@ if __name__ == "__main__":
 	replaceVersion(sys.argv[1], sys.argv[2])
 	nugetPack(sys.argv[1])
 	copyNupkg(sys.argv[3], sys.argv[2])
+
+
 
 
