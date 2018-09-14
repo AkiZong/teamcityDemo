0ddf058d9673ff7e8661fd473bd986403fd4ef39
Bingxia Zong <zbxaki@gmail.com>
2018-09-14 12:52:25 -0400

    Update test.py

--- test.py
+++ test.py
@@ -11,13 +11,14 @@ from os import fdopen, remove
 import re
 
 def main():
-    f = open("test.txt", "w+")
-    f.write("This is test.")
-    f.close()
     
     # clean output directory
     if os.path.exists('Test'):
         shutil.rmtree('Test')
+        
+    f = open("test21.txt", "w+")
+    f.write("This is test for branch 2.1.")
+    f.close()
 
 
 if __name__== "__main__":
